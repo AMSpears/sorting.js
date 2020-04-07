40b289b6f6f4d8086ed2dfb492718b9f21e9b21f
Angie Spears <angie.spears809@gmail.com>
2020-04-07 15:06:04 -0400

    add split solution

---
+++ mergesort.js
@@ -0,0 +1,17 @@
+function split(wholeArray) {
+  let firstHalf;
+  let secondHalf;
+  /* your code here to define the firstHalf and secondHalf arrays */
+  if (wholeArray.length % 2 === 0) {
+    let halfIndex = wholeArray.length / 2
+    firstHalf = wholeArray.slice(0, wholeArray[halfIndex] - 1)
+    secondHalf = wholeArray.slice(halfIndex)
+    console.log(firstHalf)
+    console.log(secondHalf)
+  } else {
+    let middle = Math.round(wholeArray.length / 2)
+    firstHalf = wholeArray.slice(0, wholeArray[middle] - 1)
+    secondHalf = wholeArray.slice(middle)
+  }
+  return [firstHalf, secondHalf];
+}
