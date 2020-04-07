93f5ceeb1be94b47de68f9f6624d4c24f1669f59
Angie Spears <angie.spears809@gmail.com>
2020-04-07 13:05:58 -0400

    initial attempt

--- bubblesort.js
+++ bubblesort.js
@@ -1,4 +1,17 @@
 const bubbleSort = (arr) => {
-// comment
-  return arr
+  let orderedArr = arr.slice()
+  for (let i = 0; i < arr.length; i++) {
+    let curr = arr[i]
+    let next = arr[i + 1]
+    let currOrder = orderedArr[i]
+    let nextOrder = orderedArr[i + 1]
+
+    // TODO: figure out how to repeately compare curr and next till all is sorted into the ordered arr
+    if (curr < next) {
+      orderedArr.splice(nextOrder, currOrder)
+    }
+  }
+
+  console.log(orderedArr)
+  return orderedArr
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -8,4 +8,7 @@ describe('Bubble Sort', function () {
   it('handles an array with a multiple items', function () {
     expect(bubbleSort([1,2,3,4,5])).toEqual([1,2,3,4,5])
   });
+  it('handles an unordered array and sorts it', function () {
+    expect(bubbleSort([4, 5, 2, 3, 1])).toEqual([1, 2, 3, 4, 5])
+  });
 });
