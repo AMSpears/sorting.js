66546e7a5c9470b1da31d89e98ad87b245fee36b
Angie Spears <angie.spears809@gmail.com>
2020-04-07 14:39:56 -0400

    add new files

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,6 @@
+// followed example from article below
+//https://medium.com/javascript-algorithms/javascript-algorithms-bubble-sort-3d27f285c3b2
+
 const bubbleSort = (arr) => {
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
