021feb87f6686f12e1619bfe3f1b2f846884af29
Angie Spears <angie.spears809@gmail.com>
2020-04-07 15:29:01 -0400

    complete combine sorted arrays

--- mergesort.js
+++ mergesort.js
@@ -6,12 +6,17 @@ function split(wholeArray) {
     let halfIndex = wholeArray.length / 2
     firstHalf = wholeArray.slice(0, wholeArray[halfIndex] - 1)
     secondHalf = wholeArray.slice(halfIndex)
-    console.log(firstHalf)
-    console.log(secondHalf)
+    // console.log(firstHalf)
+    // console.log(secondHalf)
   } else {
     let middle = Math.round(wholeArray.length / 2)
     firstHalf = wholeArray.slice(0, wholeArray[middle] - 1)
     secondHalf = wholeArray.slice(middle)
   }
   return [firstHalf, secondHalf];
+}
+
+
+function merge(firstArr, secondArr) {
+  return [...firstArr, ...secondArr]
 }
