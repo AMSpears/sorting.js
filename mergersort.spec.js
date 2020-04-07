071741a664dc7d18a03d83e7ef20685e00bc8695
Angie Spears <angie.spears809@gmail.com>
2020-04-07 15:07:59 -0400

    fix split specs

--- mergersort.spec.js
+++ mergersort.spec.js
@@ -1,10 +1,8 @@
 describe('Split Array function', function () {
   it('is able to split an array into two halves', function () {
-    it('is able to split an even array into two halves', function() {
-      expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3],[4, 5, 6]])
-    })
-    it('is able to split an odd array into two halves', function() {
-      expect(split([1, 2, 3, 4, 5, 6, 7])).toEqual([[1, 2, 3, 4],[5, 6, 7]])
-    })
-  });
+    expect(split([1, 2, 3, 4, 5, 6])).toEqual([[1, 2, 3],[4, 5, 6]])
+  })
+  it('is able to split an odd array into two halves', function() {
+    expect(split([1, 2, 3, 4, 5, 6, 7])).toEqual([[1, 2, 3, 4],[5, 6, 7]])
+  })
 });
