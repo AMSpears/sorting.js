7d556c2474a1f29c782b1fdbe300e89ed54b18cf
Angie Spears <angie.spears809@gmail.com>
2020-04-07 15:10:37 -0400

    Add spec for merge func

--- mergersort.spec.js
+++ mergersort.spec.js
@@ -6,3 +6,9 @@ describe('Split Array function', function () {
     expect(split([1, 2, 3, 4, 5, 6, 7])).toEqual([[1, 2, 3, 4],[5, 6, 7]])
   })
 });
+describe('Merge function', function () {
+  it('is able to merge two sorted arrays into one sorted array', function () {
+    expect(merge([3,2,4,1,5,6])).toEqual([1,2,3,4,5,6])
+    expect(merge([7, 3, 2, 4, 1, 5, 6])).toEqual([1, 2, 3, 4, 5, 6, 7])
+  });
+});
